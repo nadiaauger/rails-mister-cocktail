d9327a8acec810f9e54149a496694a1821c8aca9
Nadia Auger <nadia.auger0@gmail.com>
2019-11-14 16:26:36 +0100

    end done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,17 @@
-# require 'open-uri'
+require 'open-uri'
 
-# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-# ingredient_serialized = open(url).read
-# ingredient = JSON.parse(ingredient_serialized)
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredient_serialized = open(url).read
+ingredient = JSON.parse(ingredient_serialized)
 
-# p ingredient
+ingredient["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient.values[0])
+end
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "sugar")
-Ingredient.create(name: "rhum")
-Ingredient.create(name: "vodka")
-Ingredient.create(name: "ginger beer")
+# Ingredient.create(name: "lemon")
+# Ingredient.create(name: "ice")
+# Ingredient.create(name: "mint leaves")
+# Ingredient.create(name: "sugar")
+# Ingredient.create(name: "rhum")
+# Ingredient.create(name: "vodka")
+# Ingredient.create(name: "ginger beer")
