7f62a5f6865187c805baad197fca0fd46e9b07ab
Nadia Auger <nadia.auger0@gmail.com>
2019-11-15 15:50:21 +0100

    ok

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -27,6 +27,6 @@ class CocktailsController < ApplicationController
   end
 
   def set_params
-    params.require(:cocktail).permit(:name)
+    params.require(:cocktail).permit(:name, :photo)
   end
 end
