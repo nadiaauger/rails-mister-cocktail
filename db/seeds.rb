70d35f018f7f732413a168d4c4918858962f9759
Nadia Auger <nadia.auger0@gmail.com>
2019-11-15 14:53:56 +0100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
-require 'open-uri'
+# require 'open-uri'
+# ingredient.destroy
+# url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+# ingredient_serialized = open(url).read
+# ingredient = JSON.parse(ingredient_serialized)
 
-url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
-ingredient_serialized = open(url).read
-ingredient = JSON.parse(ingredient_serialized)
-
-ingredient["drinks"].each do |ingredient|
-  Ingredient.create(name: ingredient.values[0])
-end
+# ingredient["drinks"].each do |ingredient|
+#   Ingredient.create(name: ingredient.values[0])
+# end
 
 # Ingredient.create(name: "lemon")
 # Ingredient.create(name: "ice")
